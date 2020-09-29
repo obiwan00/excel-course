6215f42e724702017fb172d930429b55a8d2a5be
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2020-09-30 01:47:56 +0300

    Test phpshtorm gui for git

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -11,7 +11,7 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      // // debug
+      // debug test
       // if (component.name) {
       //   window['c' + component.name] = component
       // }
